fb32582d44da2e526e10d29e42d2ac28b5e1d1c8
Haydn King <hjking734@gmail.com>
2014-11-07 14:16:08 +0000

    Melting: reaction settings API written

--- test/test_melting.js
+++ test/test_melting.js
@@ -24,11 +24,67 @@
 
 module('seqJS.melt');
 
+var rounded = function(f){
+    return Math.round(10 * f) / 10.0;
+};
+
 test('simple melting', function(){
     //equal(seqJS.melt('ATCGATGGCATGCTAGCTGA', 'oligocalc'), 89.4);
     //equal(seqJS.melt('ATCGATGG', 'oligocalc'), 50.2);
-    equal(seqJS.melt('GCTAGC', 'Allawi1997'), 49.1);
+    equal(rounded(seqJS.Melt.melt('GCTAGC', 'Allawi1997')), 49.1);
     //equal(seqJS.melt('GCTAGC'), 32.6);
+});
+
+test('datasets', function(){
+    deepEqual(seqJS.Melt.getDatasets().sort(), ['SantaLucia1996', 'Allawi1997', 'oligocalc',].sort());
+});
+
+test('default dataset', function(){
+    equal(seqJS.Melt.getDefaultDataset(), 'Allawi1997');
+});
+
+test('getReactionParameters', function() {
+    var d = {
+        Na: 1000.0,
+        K: 0.0,
+        Tris: 0.0,
+        Mg: 0.0,
+        dNTP: 0.0,
+        Oligo: 0.5,
+    }, 
+    d2 = {
+        Na: 1000.0,
+        K: 0.0,
+        Tris: 0.0,
+        Mg: 1.0,
+        dNTP: 0.0,
+        Oligo: 0.5,
+    },
+    p = {
+        Phusion: {
+            Na: 50.0,
+            K: 0.0,
+            Tris: 0.0,
+            Mg: 1.5,
+            dNTP: 0.2,
+            Oligo: 0.5,
+        },
+        Q5: {
+            Na: 50.0,
+            K: 0.0,
+            Tris: 0.0,
+            Mg: 2.0,
+            dNTP: 0.2,
+            Oligo: 0.5,
+        },
+    };
+    deepEqual(seqJS.Melt.getReactionParameters(), d);
+    deepEqual(seqJS.Melt.getReactionParameters({}), d);
+    deepEqual(seqJS.Melt.getReactionParameters({notAsetting: 3.0}), d);
+    deepEqual(seqJS.Melt.getReactionParameters({Mg: 1.0}), d2);
+    deepEqual(seqJS.Melt.getReactionParameters('Phusion'), p['Phusion']);
+    deepEqual(seqJS.Melt.getReactionParameters('Q5'), p['Q5']);
+
 });
 
 
