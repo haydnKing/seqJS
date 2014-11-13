70a18cf50372c57d67d011696b4a3e5c749b9216
Haydn King <hjking734@gmail.com>
2014-11-13 16:28:00 +0000

    Add file and dummy function for ss prediction

---
+++ src/structure.js
@@ -0,0 +1,27 @@
+/*
+ * seqJS
+ * https://github.com/haydnKing/seqJS
+ *
+ * Copyright (c) 2014 Haydn King
+ * Licensed under the MIT license.
+ */
+
+var seqJS = seqJS || {};
+
+/**
+ * @namespace seqJS.ss
+ */
+seqJS.ss = {};
+
+(function(){
+/**
+ * Estimate the secondary structure for a given sequence
+ * @param {seqJS.Seq} seq The sequence to be melted
+ * @returns {String} the predicted secondary structure in dot-bracket notation
+ */
+seqJS.ss.predict = function(/*seq*/){
+    return "NOT_IMPLEMENTED";
+};
+
+
+}());
