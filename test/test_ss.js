323076d63cfa93ba0da7eaadd66ae8ae813d7c19
Haydn King <hjking734@gmail.com>
2014-11-13 16:16:35 +0000

    Add simple test for ss

---
+++ test/test_ss.js
@@ -0,0 +1,33 @@
+/*global seqJS:true  */
+
+(function() {
+  /*
+    ======== A Handy Little QUnit Reference ========
+    http://api.qunitjs.com/
+
+    Test methods:
+      module(name, {[setup][ ,teardown]})
+      test(name, callback)
+      expect(numberOfequalions)
+      stop(increment)
+      start(decrement)
+    Test equalions:
+      ok(value, [message])
+      equal(actual, expected, [message])
+      notEqual(actual, expected, [message])
+      deepEqual(actual, expected, [message])
+      notDeepEqual(actual, expected, [message])
+      strictEqual(actual, expected, [message])
+      notStrictEqual(actual, expected, [message])
+      throws(block, [expected], [message])
+  */
+
+module('seqJS.ss');
+
+test('Test ss', function(){
+    equal(seqJS.ss.predict('GGGAAATCC'), '.(((..)))', 'secondary structure incorrect');
+});
+
+}());
+
+
